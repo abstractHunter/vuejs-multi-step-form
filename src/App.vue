c65cd83cff646dc847cfbc3c2846d8e89ecaee74
milkiyd <milkiyd@bugger.loc>
2023-03-05 12:28:13 +0000

    add select plan component

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import TheSideBar from '@/components/TheSideBar.vue';
+import Content from '@/components/Content.vue';
+import YourInfo from '@/components/YourInfo.vue';
+import SelectPlan from '@/components/SelectPlan.vue';
+import AddOns from '@/components/AddOns.vue';
+import Summary from '@/components/Summary.vue';
+import ThankYou from '@/components/ThankYou.vue';
+import { useStepStore } from '@/stores/step';
+
+const stepStore = useStepStore();
+
+</script>
+
+<template>
+  <div class="container">
+    <TheSideBar />
+    <Content>
+      <YourInfo v-if="stepStore.step == 1" />
+      <SelectPlan v-if="stepStore.step == 2" />
+      <AddOns v-if="stepStore.step == 3" />
+      <Summary v-if="stepStore.step == 4" />
+      <ThankYou v-if="stepStore.step == 5" />
+    </Content>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 1080px;
+  height: 720px;
+  background-color: var(--alabaster);
+  border-radius: 16px;
+  padding: 20px;
+  box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.3);
+  display: flex;
+}
+</style>
---
+++ src/components/SelectPlan.vue
@@ -0,0 +1,187 @@
+<script setup>
+import { ref, computed } from 'vue';
+import IconArcade from '../assets/images/icon-arcade.svg';
+import IconAdvanced from '../assets/images/icon-advanced.svg';
+import IconPro from '../assets/images/icon-pro.svg';
+
+const checked = ref(false);
+
+const duration = computed(() => {
+    return checked.value ? 'yr' : 'mo';
+})
+
+const plans = [
+    {
+        id: 1,
+        name: 'Arcade',
+        icon: IconArcade,
+        price: {
+            mo: 9,
+            yr: 90
+        },
+    },
+    {
+        id: 2,
+        name: 'Advanced',
+        icon: IconAdvanced,
+        price: {
+            mo: 12,
+            yr: 120
+        },
+    },
+    {
+        id: 3,
+        name: 'Pro',
+        icon: IconPro,
+        price: {
+            mo: 15,
+            yr: 150
+        },
+    },
+]
+</script>
+
+<template>
+    <div>
+        <h2 class="title">Select your plan</h2>
+        <p class="subtitle">You have the option of monthly or yearly billing.</p>
+        <div class="plans">
+            <div v-for="plan in plans" :key="plan.id" class="plan">
+                <img :src="plan.icon" alt="" />
+                <h3>{{ plan.name }}</h3>
+                <p class="price">
+                    ${{ plan.price[duration] }}/{{ duration }}
+                </p>
+                <p v-if="duration == 'yr'" class="months-free">2 months free</p>
+            </div>
+        </div>
+
+        <div class="duration">
+            <label for="published">
+                Monthly
+                <input type="checkbox" v-model="checked" id="published" />
+                <span></span>
+                Yearly
+            </label>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.title {
+    font-size: 2.5rem;
+    font-weight: 600;
+    margin: 20px 0;
+    color: var(--marine-blue);
+}
+
+.subtitle {
+    font-size: 18px;
+    margin-bottom: 60px;
+    color: var(--cool-gray);
+}
+
+.plans {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 20px;
+    flex-wrap: wrap;
+}
+
+.plan {
+    width: 160px;
+    height: auto;
+    background-color: var(--white);
+    border-radius: 8px;
+    padding: 20px;
+    cursor: pointer;
+    border: 1px solid var(--light-gray);
+}
+
+.plan:hover {
+    border: 1px solid var(--purplish-blue);
+}
+
+.plan img {
+    width: 50px;
+    height: 50px;
+    margin-bottom: 50px;
+}
+
+.plan h3 {
+    font-size: 1.2rem;
+    color: var(--marine-blue);
+}
+
+
+.plan .price {
+    font-size: 1.2rem;
+    font-weight: 600;
+    color: var(--cool-gray);
+}
+
+.plan .months-free {
+    font-size: 1rem;
+    color: var(--marine-blue);
+    margin-top: 10px;
+}
+
+.duration {
+    margin-top: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 50px;
+    width: 100%;
+    background-color: var(--magnolia);
+    border-radius: 8px;
+}
+
+.duration label {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    justify-content: space-between;
+    padding: 8px;
+}
+
+.duration label input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+    height: 0;
+    width: 0;
+    display: none;
+}
+
+.duration label span {
+    position: relative;
+    display: flex;
+    align-items: center;
+    width: 50px;
+    height: 25px;
+    background-color: var(--marine-blue);
+    border-radius: 25px;
+    transition: 0.4s;
+}
+
+.duration label span:after {
+    content: '';
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    background-color: var(--white);
+    border-radius: 50%;
+    transition: 0.4s;
+    top: 2px;
+    left: 3px;
+}
+
+.duration label input:checked+span {
+    background-color: var(--marine-blue);
+}
+
+.duration label input:checked+span:after {
+    transform: translateX(25px);
+}
+</style>
