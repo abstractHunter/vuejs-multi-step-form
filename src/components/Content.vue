6945f60bddf10f8cbf87f2806fd32ab544b5c45f
milkiyd <milkiyd@bugger.loc>
2023-03-05 12:27:13 +0000

    add personnal info component

---
+++ src/components/Content.vue
@@ -0,0 +1,28 @@
+<script setup>
+import NavButtons from './NavButtons.vue';
+import { useStepStore } from '@/stores/step';
+
+const stepStore = useStepStore();
+</script>
+
+<template>
+    <div class="ct-container">
+        <div class="ct-content">
+            <slot />
+        </div>
+        <NavButtons v-if="stepStore.step < 5" />
+    </div>
+</template>
+
+<style scoped>
+.ct-container {
+    padding: 20px 100px;
+    margin-left: 20px;
+    width: calc(100% - 320px);
+}
+
+.ct-content {
+    height: 600px;
+    width: 100%;
+}
+</style>
---
+++ src/components/YourInfo.vue
@@ -0,0 +1,82 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <h2>Personal Info</h2>
+        <p>Please provide your name, email address and phone number.</p>
+
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" id="name" placeholder="e.g. Stephen King" autofocus />
+        </div>
+
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" id="email" placeholder="e.g. stephenking@lorem.com" />
+        </div>
+
+        <div class="form-group">
+            <label for="phone">Phone Number</label>
+            <input type="tel" id="phone" placeholder="e.g. +1 234 567 890" />
+        </div>
+    </div>
+</template>color: var(--marine-blue);
+
+<style scoped>
+h2 {
+    font-size: 2.5rem;
+    font-weight: 600;
+    margin: 20px 0;
+    color: var(--marine-blue);
+}
+
+p {
+    font-size: 18px;
+    margin-bottom: 60px;
+    color: var(--cool-gray);
+}
+
+.form-group {
+    margin-bottom: 40px;
+}
+
+.form-group label {
+    display: block;
+    font-size: 18px;
+    margin-bottom: 10px;
+    color: var(--marine-blue);
+}
+
+.form-group input {
+    width: 100%;
+    height: 50px;
+    border: 1px solid var(--cool-gray);
+    border-radius: 8px;
+    padding: 0 20px;
+    font-size: 18px;
+    color: var(--marine-blue);
+    padding: 24px 16px;
+}
+
+.form-group input:focus {
+    outline: none;
+    border: 1px solid var(--purplish-blue);
+}
+
+.form-group input::placeholder {
+    color: var(--cool-gray);
+}
+
+.form-group input:focus::placeholder {
+    color: var(--marine-blue);
+}
+
+.form-group input[type="email"] {
+    text-transform: lowercase;
+}
+
+.form-group input[type="email"]:focus {
+    text-transform: lowercase;
+}
+</style>
